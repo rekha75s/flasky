83b8dbf19f6f367c8e24a9e53c14ef22c3d55c7a
rekha75s <rekhapr@gmail.com>
2022-06-15 16:46:01 +0530

    initial commit

---
+++ csvread.py
@@ -0,0 +1,24 @@
+from flask import Flask,redirect,url_for,request,render_template
+
+import pandas as pd
+
+
+app=Flask(__name__)
+
+@app.route('/newbill',methods=['POST','GET'])
+def login():
+    if request.method=='POST':
+        user=request.form['nm']
+        price=request.form['price']
+        dict1={'item':[user],'price':[price]}
+        df=pd.read_csv(r"/Users/rekha/Documents/items.csv",index_col=0)
+        ind = df.tail(1).index
+        df.loc[ind[0] + 1] = [user,price]
+        print(df)
+        df.to_csv('/Users/rekha/Documents/items.csv')
+        return (render_template('user.html', name=user))
+    else:
+        user=request.args.get('nm')
+        return redirect(url_for('success',name=user))
+if __name__=='__main__':
+    app.run(debug=True)
